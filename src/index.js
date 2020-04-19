0ad0ccb5be56b16b296d024bc5664bd3a2e128f6
Spencer Eldred <eldredspencer@gmail.com>
2020-04-19 09:59:05 -1000

    Add applyMiddleware with redux dev tools

--- src/index.js
+++ src/index.js
@@ -1,16 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import { createStore } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 // import reduxThunk from 'redux-thunk';
 
 
 import App from './components/App';
 import reducers from './reducers';
 
-// const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
-const store = createStore(reducers);
+const store = createStore(
+  reducers,
+  composeEnhancers(applyMiddleware())
+);
 
 ReactDOM.render(
   <Provider store={store}>
